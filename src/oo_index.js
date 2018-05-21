77105fdeaa70bf84c95f0d957f19a257807cbaf6
SuperJesseH <therealjhorwitz@gmail.com>
2018-05-21 15:31:15 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,39 @@
+const form = document.getElementById('create-task-form');
+const tasks = document.getElementById('tasks');
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  // const taskList = new TaskList();
+
+  // figure out this arrow function at some point
+
+  form.addEventListener('submit', (e) => {
+    // prevent page load (default behavior)
+    e.preventDefault();
+    // run addTask()
+    addTask();
+    // set textbox to blank after addTask
+    form.children[1].value = ""
+  });
+
+  tasks.addEventListener("click", (e) => {
+    console.log(e.path);
+    tasks.removeChild(e.path[1])
+
+  })
+
+  function addTask() {
+    // create an element
+    // innner html should be input from form
+    // pre/append child with that input
+    let value = form.children[1].value
+    let button = document.createElement('BUTTON')
+    let newElement = document.createElement('li');
+    button.innerHTML = "X"
+    newElement.innerHTML = value;
+    newElement.append(button)
+    tasks.append(newElement);
+  }
+
+
+
 });
